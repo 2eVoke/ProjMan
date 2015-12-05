c4577b1d88f883a467a8f542093ff26d4995cca0
2eVoke <andrew@2evoke.com>
2015-12-06 00:34:11 +1000

    Still setting up.

---
+++ app/common/app.js
@@ -0,0 +1,22 @@
+var app = angular.module("app", []);
+
+app.directive("header", function () {
+	return {
+		restrict: "E",
+		templateUrl: "common/header.html"
+	};
+});
+
+
+
+
+
+
+/* DIRECTIVE DECLARATION ***************************************************
+app.directive("DIRECTIVE", function () {
+	return {
+		restrict: "E",
+		templateUrl: "TEMPLATE.html"
+	};
+});
+*/
