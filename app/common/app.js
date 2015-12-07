db211bd640683562ab415fd22a89c274fa6312a1
2eVoke <andrew@2evoke.com>
2015-12-08 02:12:22 +1000

    Still setting up.

--- app/common/app.js
+++ app/common/app.js
@@ -14,73 +14,152 @@ app.directive("content", function () {
 	};
 });
 
-app.directive("addProject", function () {
+app.directive("spinner", function () {
 	return {
 		restrict: "E",
-		templateUrl: "projects/addProject.html"
+		templateUrl: "common/spinner.html"
 	};
 });
 
-app.controller('appCtrl', ['$scope', 'projectsModel', 'notesModel', function ($scope, projectsModel, notesModel) {
-	var app = this;
-	projectsModel.getProjects()
-		.then(function (result) {
-			app.projects = result;
-		});
-	app.showSpinner = false;
-	app.toggleSpinner = function() {
-		app.showSpinner = !app.showSpinner;
-	}
+app.directive("addProject", function () {
+	return {
+		restrict: "E",
+		templateUrl: "projects/newProject.html"
+	};
+});
 
-	app.projectOrder = ['-rate', 'title'];
-	app.projectReverse = false;
-	app.projectOrderCaret = "fa-caret-down";
-	app.chProjectOrder = function (order) {
-		if (JSON.stringify(app.projectOrder) === JSON.stringify(order)) {
-			app.projectReverse = !app.projectReverse;
-		} else {
-			app.projectOrder = order;
+app.directive("editProject", function () {
+	return {
+		restrict: "E",
+		templateUrl: "projects/editProject.html"
+	};
+});
+
+app.controller('appCtrl',
+	['$scope', 'projectsModel', 'notesModel', 'newProjectModel', 'updateProjectModel', '$timeout',
+		function ($scope, projectsModel, notesModel, newProjectModel, updateProjectModel, $timeout) {
+			var app = this;
+
+			app.showSpinner = true;
+			app.toggleSpinner = function () {
+				app.showSpinner = !app.showSpinner;
+			};
+
+			app.showToast = false;
+			app.toastMessage = "I am toast";
+			app.toast = function (message) {
+				app.toastMessage = message;
+				app.showToast = true;
+				$timeout(1000)
+					.then(function () {
+						app.showToast = false;
+					});
+			};
+
+			projectsModel.getProjects()
+				.then(function (result) {
+					app.projects = result;
+					app.toggleSpinner();
+				});
+
+			app.projectOrder = ['-rate', 'title'];
 			app.projectReverse = false;
-		}
-		(app.projectReverse) ?
-			app.projectOrderCaret = "fa-caret-up" :
 			app.projectOrderCaret = "fa-caret-down";
-	};
+			app.chProjectOrder = function (order) {
+				if (JSON.stringify(app.projectOrder) === JSON.stringify(order)) {
+					app.projectReverse = !app.projectReverse;
+				} else {
+					app.projectOrder = order;
+					app.projectReverse = false;
+				}
+				(app.projectReverse) ?
+					app.projectOrderCaret = "fa-caret-up" :
+					app.projectOrderCaret = "fa-caret-down";
+			};
 
-	app.isProjectOrder = function (order) {
-		return JSON.stringify(app.projectOrder) === JSON.stringify(order);
-	};
+			app.isProjectOrder = function (order) {
+				return JSON.stringify(app.projectOrder) === JSON.stringify(order);
+			};
 
-	app.projectFilter = {'state': '1'};
-	app.chProjectFilter = function (item, value) {
-		if (app.projectFilter[item] === value) {
-			app.projectFilter[item] = '';
-		} else {
-			app.projectFilter[item] = value;
-		}
-	};
+			app.projectFilter = {'state': '1'};
+			app.chProjectFilter = function (item, value) {
+				if (app.projectFilter[item] === value) {
+					app.projectFilter[item] = '';
+				} else {
+					app.projectFilter[item] = value;
+				}
+			};
 
-	app.activeFilter = function (item, value) {
-		return app.projectFilter[item] === value;
-	};
+			app.activeFilter = function (item, value) {
+				return app.projectFilter[item] === value;
+			};
 
 
-	notesModel.getNotes()
-		.then(function (result) {
-			app.notes = result;
-		});
+			notesModel.getNotes()
+				.then(function (result) {
+					app.notes = result;
+				});
 
-	app.showAddProject = false;
-	app.toggleShowAddProject = function() {
-		app.showAddProject = !app.showAddProject;
-	};
+			app.showAddProject = false;
+			app.toggleShowAddProject = function () {
+				app.showAddProject = !app.showAddProject;
+				window.scrollTo(0, 0);
+			};
 
-	app.addProject = function(newProject) {
-		app.toggleSpinner();
-		projectsModel.addProject(newProject);
-	}
+			app.showEditProject = false;
+			app.toggleShowEditProject = function () {
+				app.showEditProject = !app.showEditProject;
+				window.scrollTo(0, 0);
+			};
 
-}]);
+			app.newProject = {};
+
+			app.addProject = function (newProject) {
+				app.toggleSpinner();
+				newProjectModel.addProject(newProject)
+					.then(function success() {
+						return projectsModel.getProjects();
+					}, function error() {
+						alert("Oops... Something went wrong!");
+					})
+					.then(function success(result) {
+						app.projects = result;
+						app.toggleShowAddProject();
+						app.toggleSpinner();
+						app.toast("Project Added");
+						app.newProject = {};
+					}, function error() {
+						alert("Oops... Something went wrong!");
+					});
+			};
+
+			app.openProject = {};
+			app.editProject = function (project) {
+				app.openProject = angular.copy(project);
+				app.toggleShowEditProject();
+			};
+
+
+			app.updateProject = function (openProject) {
+				app.toggleSpinner();
+				updateProjectModel.updateProject(openProject)
+					.then(function success() {
+						return projectsModel.getProjects();
+					}, function error() {
+						alert("Oops... Something went wrong!");
+					})
+					.then(function success(result) {
+						app.projects = result;
+						app.toggleShowEditProject();
+						app.toggleSpinner();
+						app.toast("Project Updated");
+						app.openProject = {};
+					}, function error() {
+						alert("Oops... Something went wrong!");
+					});
+			};
+
+		}]);
 
 
 /* DIRECTIVE DECLARATION ***************************************************
--- app/projects/getProjects.js
+++ app/projects/getProjects.js
@@ -9,39 +9,47 @@ projects.service('projectsModel', ['$http', function ($http) {
 			data: {"uName": "ProjManApp", "pass": "ProjMan2015"}
 		})
 			.then(function (result) {
-				var data = result.data;
-				for (var x in data) {
-					switch (data[x].rate) {
-						case '0':
-							data[x].color = "blue";
-							break;
-						case '1':
-							data[x].color = "green";
-							break;
-						case '2':
-							data[x].color = "orange";
-							break;
-						case '3':
-							data[x].color = "red";
-							break;
+					var data = result.data;
+					if (data.status === 0) {
+						console.log("No Data Returned");
+					} else {
+						for (var x in data) {
+							switch (data[x].rate) {
+								case '0':
+									data[x].color = "blue";
+									break;
+								case '1':
+									data[x].color = "green";
+									break;
+								case '2':
+									data[x].color = "orange";
+									break;
+								case '3':
+									data[x].color = "red";
+									break;
+							}
+							switch (data[x].type) {
+								case '0':
+									data[x].icon = "folder";
+									break;
+								case '1':
+									data[x].icon = "folder-o";
+									break;
+								case '2':
+									data[x].icon = "archive";
+									break;
+								case '3':
+									data[x].icon = "file";
+									break;
+							}
+						}
+						return data;
 					}
-					switch (data[x].type) {
-						case '0':
-							data[x].icon = "folder";
-							break;
-						case '1':
-							data[x].icon = "folder-o";
-							break;
-						case '2':
-							data[x].icon = "archive";
-							break;
-						case '3':
-							data[x].icon = "file";
-							break;
-					}
+				},
+				function () {
+					alert("Oops, can't seem to find the database. \n\nAre you online?");
 				}
-				return data;
-			});
+			);
 	}
 }]);
 
@@ -49,11 +57,42 @@ projects.service('newProjectModel', ['$http', function ($http) {
 	var model = this;
 
 	model.addProject = function (newProject) {
+		newProject.uName = "ProjManApp";
+		newProject.pass = "ProjMan2015";
 		return $http({
 			method: 'POST',
 			url: 'http://2evoke.com/projman/backend/newProject.php',
 			data: newProject
 		})
+
+				.then(function (result) {
+					var data = result.data;
+					if (data.status === 0) {
+						console.log("Operation Unsuccessful");
+					}
+				});
+	}
+
+}]);
+
+projects.service('updateProjectModel', ['$http', function ($http) {
+	var model = this;
+
+	model.updateProject = function (openProject) {
+		openProject.uName = "ProjManApp";
+		openProject.pass = "ProjMan2015";
+		return $http({
+			method: 'POST',
+			url: 'http://2evoke.com/projman/backend/updateProject.php',
+			data: openProject
+		})
+
+				.then(function (result) {
+					var data = result.data;
+					if (data.status === 0) {
+						console.log("Operation Unsuccessful");
+					}
+				});
 	}
 
 }]);
